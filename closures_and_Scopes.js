4cfbc584853a4c1f2c1089377fd1d8b9d5f13c70
int <yuskovss@gmail.com>
2019-10-31 01:29:08 +0400

    refactor: delete comment code in task Closures and Scopes

--- closures_and_Scopes.js
+++ closures_and_Scopes.js
@@ -1,26 +1,9 @@
 function createFunctions(n) {
     var callbacks = [];
-    for (let i = 0; i < n; i++) {       
+    for (let i = 0; i < n; i++) {
         callbacks.push(function () {
             return i;
         });
     }
     return callbacks;
 }
-
-var callbacks = createFunctions(5);
-console.log(callbacks[0]());
-i = 2;
-console.log(callbacks[1]());
-// console.log(callbacks[2]());
-// console.log(callbacks[3]());
-/*
-function createCounter() {
-    let counter = 0;
-    const myFunction = function() {
-      counter = counter + 1;
-      return counter;
-    }
-    return myFunction;
-  }
-  */
